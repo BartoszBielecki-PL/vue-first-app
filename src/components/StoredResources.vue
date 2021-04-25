103e963ddd51affbac8f3c362badc3923342a258
Bartosz Bielecki <Bartosz.m.Bielecki@gmail.com>
2021-04-25 21:22:16 +0200

    fist app done

---
+++ src/components/StoredResources.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul>
+    <resource-item
+      v-for="item in resources"
+      :key="item.id"
+      :id="item.id"
+      :title="item.title"
+      :description="item.description"
+      :link="item.link"
+    ></resource-item>
+  </ul>
+</template>
+
+<script>
+import ResourceItem from "./ResourceItem.vue";
+export default {
+  inject: ["resources"],
+  components: {
+    ResourceItem,
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
